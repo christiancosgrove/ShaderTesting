8cad9962e8fc6c84f11e0417f69270708ca92302
Christian Cosgrove <christian.cosgrove@gmail.com>
2015-05-12 11:36:06 -0400

    Initial Commit

---
+++ ShaderTesting/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ShaderTesting
+//
+//  Created by Christian Cosgrove on 5/12/15.
+//  Copyright (c) 2015 Christian Cosgrove. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
